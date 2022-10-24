6b9ad75d18aa6c19f898425f712a771e0add968a
HHrach <heatherhrach@gmail.com>
2022-10-24 00:56:37 -0700

    Matrix Inverse submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#set and get the value of a matrix, and then set and get the inverse of that matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invrs <- NULL
+  set <- function(y){
+    x <<- y
+    invrs <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) invrs <<- inverse
+  getinverse <- function() invrs
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+#Check to see if matrix inverse has already been calculated and cached. If not, calculate the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invrs <- x$getinverse()
+  if(!is.null(invrs)){
+    message("getting cached data")
+    return(invrs)
+  }
+  data <- x$get()
+  invrs <- inverse(data, ...)
+  x$setinverse(invrs)
+  invrs
 }
+
